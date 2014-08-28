7fccbb81a43b4af07160525b2a41d6649a96c849
Christian Horsdal <c.horsdal@gmail.com>
2014-08-28 22:59:31 +0200

    add test project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+  using SeamExercise;
+
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod, ExpectedException(typeof(ArgumentException))]
+    public void TestMethod1()
+    {
+      var s = new UrlShorteningService();
+      s.ShortenUrl("foo");
+    }
+  }
+}
